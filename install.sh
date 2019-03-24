eeb03d1183e15cf09b22a1a5c58b81c78fafe7b5
stepanhajek <48883476+stepanhajek@users.noreply.github.com>
2019-03-24 15:42:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ echo ================web, multimedia==================;
 echo ================web, multimedia==================;
 
 # web, multimedia
-sudo apt-get install chromium vlc vim;
+sudo apt-get install chromium;
+sudo apt-get install vlc;
+sudo apt-get vim;
 
 echo ================atom editor==================;
 echo ================atom editor==================;
@@ -26,14 +28,3 @@ sudo apt-get install atom;
 echo ================java==================;
 echo ================java==================;
 echo ================java==================;
-
-# java
-sudo apt-get install software-properties-common;
-sudo add-apt-repository ppa:webupd8team/java;
-sudo apt-get update;
-sudo apt-get install oracle-java8-installer;
-sudo add-apt-repository -r ppa:webupd8team/java;
-
-#open jdk
-sudo apt-get install openjdk-8-jdk;
-sudo apt-get install openjdk-8-jre;
