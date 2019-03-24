ae47418281376f08ae525696a32f272dc9b9dfd4
stepanhajek <48883476+stepanhajek@users.noreply.github.com>
2019-03-24 16:10:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ echo ================update, upgrade==================;
 echo ================update, upgrade==================;
 
 # update, upgrade
-sudo apt-get update && sudo apt-get upgrade;
+sudo apt-get update && sudo apt-get upgrade -y;
 
 echo ================web, multimedia==================;
 echo ================web, multimedia==================;
 echo ================web, multimedia==================;
 
 # web, multimedia
-sudo apt-get install chromium;
-sudo apt-get install vlc;
-sudo apt-get vim;
+sudo apt-get install chromium-browser -y;
+sudo apt-get install vlc -y;
+sudo apt-get install vim -y;
 
 echo ================atom editor==================;
 echo ================atom editor==================;
@@ -23,8 +23,12 @@ echo ================atom editor==================;
 # atom editor
 sudo add-apt-repository ppa:webupd8team/atom;
 sudo apt-get update;
-sudo apt-get install atom;
+sudo apt-get install atom -y;
 
-echo ================java==================;
-echo ================java==================;
-echo ================java==================;
+
+echo ================ GIMP ==================;
+echo ================ GIMP ==================;
+echo ================ GIMP ==================;
+
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp-edge;
+sudo apt update && sudo apt install gimp gimp-gmic -y;
